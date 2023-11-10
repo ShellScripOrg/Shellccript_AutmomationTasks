8238d0cfb2920f8ba685346ef55917d46d2498ec
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-10 12:06:10 +0000

    Revert "updated"
    This reverts commit cdf7d7e517e1530b324c93c9fd433ab1a1de3b58.
    
    Below is code issue i am reverting this file

--- DbUtils.java
+++ DbUtils.java
@@ -3,9 +3,3 @@ Again I am fetching this file in working area
 Again we are testing this filr
 I am making Db changes in this file 
 after that i will run commnad -> git push aliasname branchname
-
-Hello I need to remove this below code
-#!/bin/bash
-echo 
-echo sddg
-
