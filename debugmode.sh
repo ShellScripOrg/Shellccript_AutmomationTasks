01db599363f96645e57360cee49b3424c05023e2
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-11 10:04:46 +0000

    1stcommit

---
+++ debugmode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Purpose: Checking debug mode options
+#Author: Ajay Jamliya
+#Modified : 22/10/2023
+
+echo 
+echo enter the two number by cmd 
+echo 
+sum=`expr $1 + $2`
+echo sum is $sum 
+echo done !!
