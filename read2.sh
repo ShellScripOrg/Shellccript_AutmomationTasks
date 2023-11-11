b9e1d4c433eff57c87f49994816fa4845dc45263
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-11 09:51:08 +0000

    1stcommit

---
+++ read2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose: read comnd demo
+#Author: Ajay Jamliya
+#Modified: 10/10/2023
+
+echo 
+echo enter the DevOps tool
+echo 
+read t1 t2 t3
+echo tools are $t1 , $t2 , $t3
+echo 
+echo end !
---
+++ read3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose:read command
+#Author: Ajay Jamliya
+#Modified: 21/10/2023
+
+echo 
+echo Please enter the devops tools
+echo 
+read -a tools
+echo Tools entered by you is ${tools[0]} , ${tools[1]}
+echo 
+echo end !!
