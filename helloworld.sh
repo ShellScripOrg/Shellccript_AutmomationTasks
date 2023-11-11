9d5142af44452c4c29fc74bcf1c0423fac3cc359
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-11 11:11:14 +0000

    1stcommit

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author: Ajay Jamliya
+#Date: 26/oct/2023
+#Modification: 26/oct/2023
+#Description: This script will print hello world
+
+echo 
+echo Hello World
+echo 
---
+++ runbasictasks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author: Ajay- Jamliya
+#Date: 26/oct/2023
+#Modification: 26/oct/2023
+#Description: This script will run basic tasks
+
+echo 
+echo We are running basic tasks
+pwd
+ls 
+whoami
+cal
+date 
+touch a b c
+echo done !!
